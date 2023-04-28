0298e4203cde606a0b9f66118bfb98ba67f045a3
George <george.gui2000@gmail.com>
2023-04-28 18:13:24 -0300

    feat: adiciona cor nas pecas

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ try
 {
     TabuleiroC tab = new TabuleiroC(8, 8);
 
-    tab.PosicionarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
+    tab.PosicionarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 1));
     tab.PosicionarPeca(new Torre(Cor.Preto, tab), new Posicao(1, 3));
-    tab.PosicionarPeca(new Rei(Cor.Preto, tab), new Posicao(2, 4));
+    tab.PosicionarPeca(new Rei(Cor.Preto, tab), new Posicao(0, 2));
+
+
+    tab.PosicionarPeca(new Rei(Cor.Branco, tab), new Posicao(3, 5));
 
     Tela.ImprimirTabuleiro(tab);
 
---
+++ Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole.Xadrez;
+
+public class PosicaoXadrez
+{
+    public int Linha { get; set; }
+
+    public char Coluna { get; set; }
+
+    public PosicaoXadrez(char coluna, int linha)
+    {
+        Coluna = coluna;
+        Linha = linha;
+    }
+
+    public Posicao toPosicao()
+    {
+        return new Posicao(8 - Linha, Coluna - 'a');
+    }
+
+    public override string ToString()
+    {
+        return $"{Coluna}{Linha}";
+    }
+}
